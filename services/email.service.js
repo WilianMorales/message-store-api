ffd91aea12db74327b25e5982c977c09e08b85f1
Wilian Morales <willian.moralesch@gmail.com>
2025-04-20 00:16:04 -0500

    fix: configurar Nodemailer para usar Gmail como servicio de correo

--- services/email.service.js
+++ services/email.service.js
@@ -4,9 +4,7 @@ dotenv.config();
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT),
-    secure: process.env.SMTP_SECURE === 'true',
+    service: 'gmail',
     auth: {
         user: process.env.SMTP_USER,   // Tu email
         pass: process.env.SMTP_PASS    // Tu contraseña o app password
